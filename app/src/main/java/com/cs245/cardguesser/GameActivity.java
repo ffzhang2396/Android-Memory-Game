effcb40d1765a4cf775af7fdc0e9eccea9477e0c
Felix Zhang <ffzhang@cpp.edu>
2017-11-30 10:53:24 -0800

    Fixed some stuff

--- app/src/main/java/com/cs245/cardguesser/GameActivity.java
+++ app/src/main/java/com/cs245/cardguesser/GameActivity.java
@@ -1,5 +1,6 @@
 package com.cs245.cardguesser;
 
+import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Handler;
 import android.support.annotation.RequiresApi;
@@ -7,14 +8,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.GridLayout;
 import android.widget.GridView;
-import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.Collections;
 
-public class Game4x1Activity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class GameActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private int numberOfElements;
     private int numberOfRows;
@@ -33,7 +32,7 @@ public class Game4x1Activity extends AppCompatActivity implements AdapterView.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game4x1);
+        setContentView(R.layout.activity_game);
 
         this.numberOfElements = getIntent().getIntExtra("numberOfCards", 0);
 
@@ -129,6 +128,7 @@ public class Game4x1Activity extends AppCompatActivity implements AdapterView.On
 
         }
     }
+
 
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
